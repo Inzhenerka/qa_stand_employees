831ee3b02eece29487da4a496357c583cde46304
Pavel Roslovets <p.v.roslovets@gmail.com>
2025-07-27 10:52:05 +0200

    fix: backend url

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
-export const API_URL = "https://x-clients-be.onrender.com";
+export const API_URL = "https://x-clients-be.inzhenerka-cloud.ru";
 export const API_DOCS_URL = API_URL + "/docs";
 export const REPO_URL = "https://github.com/Inzhenerka/qa_stand_employees"
